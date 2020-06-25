6beb95fbd11ff2c160c953093e47a6199fad324b
Hanka <hankakokocinska@wp.pl>
2020-06-25 10:26:17 +0200

    change of database structure and views 2

---
+++ samochody/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm
+from .models import Cars, Models, Fuels
+
+
+class CarForm(ModelForm):
+    class Meta:
+        model = Cars
+        fields = ( "id_car", "production_year", "price", "date_ad", "mileage", "capacity", "new_price")
+
+class ModelForm(ModelForm):
+    class Meta:
+        model = Models
+        fields = ["model"]
+
+class FuelForm(ModelForm):
+    class Meta:
+        model = Fuels
+        fields = ["fuel"]
+
+
+
